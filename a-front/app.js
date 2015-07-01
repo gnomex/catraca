4136624b3fc39fb501bd38df88dc4f980e85e0f8
Kenner Kliemann <kenner.hp@gmail.com>
2015-07-01 15:26:46 -0300

    Working with events :+1:

--- a-front/app.js
+++ a-front/app.js
@@ -14,17 +14,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'bower')));
 app.use('/vendor', express.static(__dirname + '/bower/vendor'));
-
 app.use('/', routes);
 
 // catch 404 and forward to error handler
@@ -57,6 +52,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
